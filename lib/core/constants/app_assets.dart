3fdc1564bf9fb22a377d3a753117a757792ec68d
Nguyễn Đình Linh <linhngdeveloper@maplelabs.co>
2025-01-31 12:28:33 +0700

    new ui

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String imagesBgHome = "assets/images/bg_home.png";
+
+  static const String imagesIcClear = "assets/images/ic_clear.png";
+
+  static const String imagesIcHumidity = "assets/images/ic_humidity.png";
+
+  static const String imagesIcLocation = "assets/images/ic_location.png";
+
+  static const String imagesIcMenu = "assets/images/ic_menu.png";
+
+  static const String imagesIcRain = "assets/images/ic_rain.png";
+
+  static const String imagesIcSun = "assets/images/ic_sun.png";
+
+  static const String imagesIcWind = "assets/images/ic_wind.png";
+}
+
--- lib/core/constants/export.dart
+++ lib/core/constants/export.dart
@@ -1,4 +1,4 @@
 export 'colors.dart';
 export 'dimens.dart';
 export 'font.dart';
-export 'icon.dart';
+export 'app_assets.dart';
